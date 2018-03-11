58c657cce9f681356b2707724dd85bae743264fd
Robostorm <robostormweb@gmail.com>
2018-03-10 21:42:11 -0500

    Add root directory

---
+++ root/header.php
@@ -0,0 +1,44 @@
+<nav class="navbar navbar-static-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand">
+                <img alt="" src="assets/banner.png">
+            </a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Home</a></li>
+              <li>
+                    <a href="projects.php" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                       aria-expanded="false">Projects <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="aquabot.php">Aquabot</a></li>
+                        <li><a href="mazebots.php">MazeBots</a></li>
+                        <li><a href="3dprinter.php">3d Printer</a></li>
+                        <li><a href="arcade.php">Arcade Machine</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="members.php" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                       aria-expanded="false">Members <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="http://forum.robostorm.org/" target="_blank">Forum</a></li>
+                        <li><a href="http://projects.robostorm.org/" target="_blank">Project Management</a></li>
+                    </ul>
+                </li>
+                <li><a href="battlebots.php">Battlebots </a></li>
+                
+                <li><a href="documents.php">Documents</a></li>
+                <li><a href="events.php">Events</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
